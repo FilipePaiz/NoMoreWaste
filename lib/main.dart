bd02326b976cd7705209ef86f73dd5a392a47cd2
Filipe Pais <vitor.filipe.pais@gmail.com>
2020-04-23 01:07:35 +0100

    added new library for date to strings

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:expenses_app/transaction.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -84,7 +85,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat().format(tx.date),
                           style: TextStyle(color: Colors.grey),
                         ),
                       ],
